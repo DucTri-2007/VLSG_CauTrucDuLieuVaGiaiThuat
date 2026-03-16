cee6f006c477c768b9d6cdc16a742a5f170840d0
VuNguyenDucTri <ductrivunguyen@gmail.com>
2026-03-16 10:23:43 +0700

    Hoan Thanh BVN Buoi 2: De quy va So Sanh bo nho

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DSA_Homework_S2
+{
+    class Program
+    {
+        static int TinhTong(int n)
+        {
+            if (n == 1) return 1;
+            return n + TinhTong(n - 1);
+        }
+
+        static long GiaiThuaVongLap(int n)
+        {
+            long KetQua = 1;
+            for (int i = 1; i <= n; i++) 
+            {
+                KetQua *= i;
+            }
+            return KetQua;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Nhap n de tinh tong: ");    
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Tong S{(n)} = {TinhTong(n)}");
+            Console.WriteLine($"Giai thua cua {n} la {GiaiThuaVongLap(n)}");
+        }
+    }
+}
