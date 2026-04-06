81fd90c7e4e172584d58313aff7488b2b233115a
VuNguyenDucTri <ductrivunguyen@gmail.com>
2026-04-06 09:51:22 +0700

    Khoi tao cau truc Buoi 06.02

---
+++ DSA_Session06_Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSA_Session06_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DSA_Session06_Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSA_Session06_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
